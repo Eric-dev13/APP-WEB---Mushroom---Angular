326f4cc12292d7e21daa7ba4471fadf120d5681f
Eric-dev13 <lanzae32@gmail.com>
2023-11-21 22:51:44 +0100

    Feat(authentication): création du composant pour changer le mot de passe en cours #71

--- src/app/pages/authentication/user/editer-password/editer-password.component.ts
+++ src/app/pages/authentication/user/editer-password/editer-password.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Form } from '@angular/forms';
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-editer-password',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./editer-password.component.scss']
 })
 export class EditerPasswordComponent {
+  
+  faEye= faEye;
 
+  togglePasswordVisibility = () => {
+    const passwordInput = document.getElementById("password");
+
+    if (passwordInput) {
+      const currentType = passwordInput.getAttribute("type");
+      const newType = currentType === "password" ? "text" : "password";
+      passwordInput.setAttribute("type", newType);
+    }
+  }
+
+  send = (form:Form) => {
+
+  }
 }
